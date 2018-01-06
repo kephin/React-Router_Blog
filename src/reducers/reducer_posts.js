738e0454cbc7885f1926fd8d65ec19df1b54f9c9
kephin <wunshon76325@gmail.com>
2018-01-06 16:32:56 +0800

    replace lodash with native ES6 method

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,13 @@
-import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
+
+const convertArrayToObject = (array, key) => array.reduce((object, item) => {
+  return { ...object, [item[key]]: item };
+}, {});
 
 export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      return _.mapKeys(action.payload.data, 'id');
+      return convertArrayToObject(action.payload.data, 'id');
 
     default:
       return state;
