3c5f6abf945f0eeddf03e3efab89019bb938b554
kephin <wunshon76325@gmail.com>
2018-01-06 16:47:11 +0800

    fix typos and format

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POSTS = 'FETCH_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=thisisatest';
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-  
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);  
   return {
     type: FETCH_POSTS,
     payload: request,
