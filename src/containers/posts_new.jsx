2c7d3c229c31c471cf424456b0ab92d7af684f41
kephin <wunshon76325@gmail.com>
2018-01-20 15:20:24 +0800

    add field components inside posts_new

--- src/containers/posts_new.jsx
+++ src/containers/posts_new.jsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
 
+// the function "reduxForm" is identical to "connect" helper in react-redux library
+
+// the field component only knows how to wire up all the event handlers, action creators, etc.
+// but it doesn't know how to produce JSX on the screen to our users
+import { Field, reduxForm } from 'redux-form';
+
 class PostsNew extends Component {
+  // need to pass the field argument, for 2 reasons:
+  // 1. field object contains event handlers that we need to wire up to the JSX that we are returning
+  // 2. in order to make sure that the Field component knows which input text it is responsible for
+  renderField(field) {
+    return (
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input
+          className='form-control'
+          type='text'
+          // field.input contains onChange={...}, onFocus={...}, onBlur={...}, etc.
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>Post New!</div>
+      <form>
+        <Field
+          label='Title'
+          name='title'
+          component={this.renderField}
+        />
+        <Field
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field
+          label='Post Content'
+          name='content'
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// allow form to communicate directly from the component to the reducer
+export default reduxForm({
+  // to specify the unique name for this particular form
+  form: 'PostsNewForm',
+})(PostsNew);
