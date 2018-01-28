5bee48af66681a6534f5d68fa10948c50f730d9a
kephin <wunshon76325@gmail.com>
2018-01-28 19:44:12 +0800

    create posts_show component

--- src/index.jsx
+++ src/index.jsx
@@ -8,15 +8,18 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostIndex from './containers/posts_index';
 import PostsNew from './containers/posts_new';
+import PostsShow from './containers/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+// Be careful that the order of our routes matters!
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Switch>
           <Route path='/posts/new' component={PostsNew} />
+          <Route path='/posts/:id' component={PostsShow} />
           <Route path='/' component={PostIndex} />
         </Switch>
       </div>
