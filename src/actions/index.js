cb2d7b2c3b8cdfae31b521ce3cdcb459e51f7f7e
kephin <wunshon76325@gmail.com>
2018-01-28 19:55:37 +0800

    add action creator and reducer for posts_new container

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=thisisatest';
@@ -10,6 +11,14 @@ export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
     type: FETCH_POSTS,
+    payload: request,
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: FETCH_POST,
     payload: request,
   };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 const convertArrayToObject = (array, key) => array
   .reduce((object, item) => ({ ...object, [item[key]]: item }), {});
@@ -7,6 +7,10 @@ export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
       return convertArrayToObject(action.payload.data, 'id');
+    case FETCH_POST:
+      // take all the posts that we've already fetched
+      // add existing application level state
+      return { ...state, [action.payload.data.id]: action.payload.data };
 
     default:
       return state;
